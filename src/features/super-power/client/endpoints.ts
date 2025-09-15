b59b8e0b113e66c91322331212a359b862e9d177
Vitalii Yaltukhov <vitalii3797@gmail.com>
2025-09-15 15:36:02 +0300

    test deployment

--- src/features/super-power/client/endpoints.ts
+++ src/features/super-power/client/endpoints.ts
@@ -1,4 +1,5 @@
 export const endpoints = {
-  baseUrl: import.meta.env.VITE_SUPERHERO_API,
+  //   baseUrl: import.meta.env.VITE_SUPERHERO_API,
+  baseUrl: "https://superhero-app-api.vercel.app/api",
   getAllSuperPowers: "/super-powers",
 };
--- src/features/superhero/client/endpoints.ts
+++ src/features/superhero/client/endpoints.ts
@@ -1,5 +1,6 @@
 export const endpoints = {
-  baseUrl: import.meta.env.VITE_SUPERHERO_API,
+  //   baseUrl: import.meta.env.VITE_SUPERHERO_API,
+  baseUrl: "https://superhero-app-api.vercel.app/api",
   getSuperheroesPaginated: "/superheroes/paginated",
   getSuperheroById: "/superheroes/",
   createSuperhero: "/superheroes",
